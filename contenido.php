560685fa03a1117fb4c869288d3f0eb8dd1edd06
noe alejandro lopez lopez <alex1_99_86@hotmail.com>
2017-10-22 23:11:33 -0500

    esta es la otra parte del contenido donde se crean los campos para recabar los datos

---
+++ contenido.php
@@ -0,0 +1,30 @@
+<section id="sitio">
+			<article class="sitios">
+				<h2>Aprender CSS3</h2>
+				<p><?php echo $p; ?></p>
+			</article>
+			<article class="sitios">
+            <h2>Crea tu propia web</h2>
+            <img src="portada.png">
+			</article>
+			<article class="sitios">
+				<h2>Colores en la web</h2>
+				<fieldset>
+					<legend>encuentra tu color</legend>
+					<form>
+						<label>Selecciona tu color</label>
+						<input type="color">
+						<br>
+						<input type="submit" value="buscar color">
+
+					</form>
+				</fieldset>
+			
+			</article>
+		</section>
+</div>
+ <footer>
+<p><?php echo $footer; ?></p>
+</footer>
+</body>
+</html>
