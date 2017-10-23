b6675ff5f52d0e6576620a4ede209363037b289a
noe alejandro lopez lopez <alex1_99_86@hotmail.com>
2017-10-22 22:58:36 -0500

    se creo la pagina header la cual contiene el encabezado de la pagina

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+        <head>
+        	<meta charset="utf8">
+	<meta name="viewport" content="width"=device-width, initial-scale=1>
+                <title>CodeIgniter Tutorial</title>
+                
+		<title>Mi pagina</title>
+		<link rel="stylesheet" type="text/css" href="<?=base_url()?>/estilo.css">
+		<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Rock+Salt|Special+Elite|VT323" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Fontdiner+Swanky" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Slackey" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet">
+
+        </head>
+        
+        <body>
+
+                <h1><?php echo $title; ?></h1>
